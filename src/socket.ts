6dd3fe286534602943665742cb7595ca20029955
Bobby <bobur0218programmer@gmail.com>
2025-01-06 15:40:57 +0500

    try again

--- src/socket.ts
+++ src/socket.ts
@@ -2,16 +2,11 @@ import { io } from "socket.io-client";
 import { BACKEND_BASE_URL } from "./constants";
 
 const socket = io(BACKEND_BASE_URL, {
-  // cert: ENV === 'production' ? SSL_CERT : '',
-  // key: ENV === 'production' ? SSL_KEY : '',
   transports: ["polling"],
   path: "/socket.io/",
   withCredentials: true,
   reconnection: true,
   reconnectionAttempts: 5,
-  extraHeaders: {
-    "Access-Control-Allow-Origin": "*",
-  },
 });
 
 export default socket;
