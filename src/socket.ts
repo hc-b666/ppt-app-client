e63ede1b4361d015bfb21bfe49f7e55f4dac2a41
Bobby <bobur0218programmer@gmail.com>
2025-01-06 15:38:05 +0500

    fix bugs

--- src/socket.ts
+++ src/socket.ts
@@ -4,11 +4,14 @@ import { BACKEND_BASE_URL } from "./constants";
 const socket = io(BACKEND_BASE_URL, {
   // cert: ENV === 'production' ? SSL_CERT : '',
   // key: ENV === 'production' ? SSL_KEY : '',
-  transports: ["websocket", "polling"],
-  path: "/socket.io",
+  transports: ["polling"],
+  path: "/socket.io/",
   withCredentials: true,
   reconnection: true,
   reconnectionAttempts: 5,
+  extraHeaders: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default socket;
